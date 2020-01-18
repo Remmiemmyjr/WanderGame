0f74017aeef2eb78201626668148fca17b71083e
Remmiemmyjr <emily.berg@digipen.edu>
2020-01-17 17:44:36 -0800

    created new project

---
+++ Wander/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float moveSpeed;
+    Vector3 toPlayer = new Vector3(0.0f, 0.0f, 0.0f);
+    Transform Player;
+    int maxDistance = 20;
+    bool following = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        moveSpeed = Random.Range(0.01f, 0.05f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.position) <= maxDistance)
+        {
+            following = true;
+        }
+        if (following)
+        {
+            Follow();
+        }
+    }
+
+    private void Follow()
+    {
+
+        toPlayer = Player.position - transform.position;
+        if (System.Math.Abs(toPlayer.x) > System.Math.Abs(toPlayer.y))
+        {
+            toPlayer.y = Random.Range(-1, 1);
+        }
+        else
+        {
+            toPlayer.x = Random.Range(-1, 1);
+        }
+
+        toPlayer = toPlayer.normalized * moveSpeed;
+        transform.position += toPlayer;
+
+
+        //transform.position += velocity * moveSpeed;
+    }
+}
---
+++ Wander/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float moveSpeedDamp = 0.5f;
+    Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+    Vector3 Destination = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    void Move()
+    {
+        velocity.y = 0;
+        velocity.x = 0;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity.y = moveSpeed * moveSpeedDamp;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity.y = moveSpeedDamp * -moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = moveSpeedDamp * -moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = moveSpeed * moveSpeedDamp;
+        }
+        transform.position += velocity;
+        velocity = velocity.normalized * moveSpeed;
+    }
+    /* bool ValidDir(Vector2 direction)
+     {
+         //draw a line from the next tile over + a little and check if the maze is in the way
+         Vector2 pos = transform.position;
+         direction += direction * 0.55f;
+         //bit shift the index of the layermask to only check layer 8
+         //which should be the maze
+         int layerMask = 1 << 8;
+         RaycastHit2D hit = Physics2D.Linecast(pos + direction, pos, layerMask);
+         return hit.collider == null;
+     } */
+}
